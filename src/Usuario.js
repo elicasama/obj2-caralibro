d0277df3a61a00f2ead9c2b8b5498232cc8315c4
Elizabeth Capuzzi <elicasama@gmail.com>
2021-06-14 19:08:08 -0300

    Se agrega la clase Usuario y los tests correspondientes

---
+++ src/Usuario.js
@@ -0,0 +1,19 @@
+//var publicaciones = require("../src/Publicacion");
+var _ = require("lodash");
+
+module.exports = class Usuario {
+  constructor() {
+    this.publicaciones = [];
+  }
+
+  agregarPublicacion(publicacion) {
+    this.publicaciones.push(publicacion);
+  }
+
+  espacioDePublicaciones() {
+    return _.sumBy(this.publicaciones, (publicacion) =>
+      publicacion.espacioQueOcupa()
+    );
+  }
+
+};
--- tests/tests.js
+++ tests/tests.js
@@ -2,21 +2,52 @@ var assert = require("assert");
 // const Publicacion = require("../src/Publicacion");
 const Foto = require("../src/Foto");
 const Texto = require("../src/Texto");
+const Usuario = require("../src/Usuario");
 
 describe("Pruebas", () => {
   describe("Compresion de archivos", () => {
     describe("Fotos", () => {
-      it("Una foto de alto = 4 ancho = 3 y un FC = 0.7 ", () => {
+      it("El espacio se calcula alto * ancho * FC, el FC = 0.7", () => {
         const foto = new Foto(4, 3);
         assert.equal(9, foto.espacioQueOcupa());
       });
     });
     describe("Texto", () => {
-        it("Una un ", () => {
-          const contenido = "Este es un contenido de prueba" // <-- tiene 30 caracteres
-          const texto = new Texto(contenido)
-          assert.equal(30, texto.espacioQueOcupa());
-        });
+      it("El espacio que ocupa es la cantidad de caracteres que tiene", () => {
+        const contenido = "Este es un contenido de prueba"; // <-- tiene 30 caracteres
+        const texto = new Texto(contenido);
+        assert.equal(30, texto.espacioQueOcupa());
       });
+    });
+  });
+  describe("Usuarios", () => {
+    describe("Publicaciones", () => {
+      it("Agrego una publicación tipo Texto", () => {
+        const contenido = "Un pequeño contenido de pocas letras"
+        const texto = new Texto (contenido); // <-- ocupa 36
+        const usuario = new Usuario();
+        usuario.agregarPublicacion(texto);
+
+        assert.equal(1, usuario.publicaciones.length);
+      });
+      it("Agrego una publicación tipo Foto", () => {
+        const foto = new Foto (3, 10); //<-- ocupa 21
+        const usuario = new Usuario();
+        usuario.agregarPublicacion(foto);
+
+        assert.equal(1, usuario.publicaciones.length);
+      });
+      it("Cálculo de espacio de las publicaciones", () => {
+        const contenido = "Un pequeño contenido de pocas letras"
+        const foto = new Foto (3, 10); //<-- ocupa 21
+        const texto = new Texto (contenido); // <-- ocupa 36
+
+        const usuario = new Usuario();
+        usuario.agregarPublicacion(foto);
+        usuario.agregarPublicacion(texto);
+
+        assert.equal(57, usuario.espacioDePublicaciones());
+      });
+    });
   });
 });
